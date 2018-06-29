1c643c95e27f8c29e9628385161827e4d3ec595f
Luke Edwards <luke.edwards05@gmail.com>
2018-06-29 16:17:58 -0700

    attach Vidy embed script & styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import zoom from 'zoom-image';
+import Vidy from './vidy';
 import './index.styl';
+import './vidy.css';
 
 let i=0, list=document.querySelectorAll('.image img');
 while (i < list.length) zoom(list[i++]);
+
+let vidy = new Vidy({
+	appid: '2199e8c8-0c59-4617-b100-d463129790c5',
+	postid: 'demo-static-grub-street',
+	content: '.content article',
+	autoload: true,
+});
